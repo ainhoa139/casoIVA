dbbcf06b22901f60f90abf896c216d201ada35c4
ainhoa139 <ai.campo@aulanz.net>
2024-11-14 15:30:44 +0100

    Add files via upload

---
+++ casoIVA.py
@@ -0,0 +1,23 @@
+print("Programa para calculo IVA")
+
+precio_sin_IVA=float(input("introduce un precio"))
+IVA =float(input("introduce el IVA"))
+
+IVA=precio_sin_IVA*IVA/100
+precio_con_IVA=precio_sin_IVA + IVA
+
+print("El precio sin IVA es " ,precio_sin_IVA)
+print("El IVA es " , IVA)
+print("El precio con IVA es " , precio_con_IVA)
+
+
+
+  
+
+
+
+
+
+            
+
+
